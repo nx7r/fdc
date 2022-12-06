154c3fa75e0538988caf3fe47b1f5e5313cdf5ef
Djawad <78606454+nx7r@users.noreply.github.com>
2022-12-06 16:00:10 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from . import forms, decorators
 
-# Create your views here.
+
 @decorators.unauthenticated
 def loginPage(request):
 	if request.method == 'POST':
